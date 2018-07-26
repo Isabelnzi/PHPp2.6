1727cad9b41c7a0e4a1806f8242632a92610196c
Nzingani <isabelnzingani@outlook.com>
2018-07-26 15:41:06 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>exercice3</title>
+  </head>
+  <body>
+    <p>
+<?php
+$age = 12;
+
+if($age >= 18)
+{
+echo 'Tu es majeur ';
+}
+elseif($age <= 18)
+{
+echo 'tu es mineur';
+}
+?>
+</p>
+</body>
